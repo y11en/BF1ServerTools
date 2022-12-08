e0877bb22cb958feed9c0a5069487e87794b94f9
穗乃果 <1763575579@qq.com>
2022-12-09 02:13:18 +0800

    update

--- BF1ServerTools.SDK/Class1.cs
+++
@@ -1,8 +0,0 @@
-using System;
-
-namespace BF1ServerTools.SDK
-{
-    public class Class1
-    {
-    }
-}
---
+++ BF1ServerTools.SDK/Using.cs
@@ -0,0 +1,5 @@
+﻿global using System;
+global using System.Text;
+global using System.Threading;
+global using System.Diagnostics;
+global using System.Runtime.InteropServices;
